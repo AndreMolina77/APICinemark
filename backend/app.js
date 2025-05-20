94ce6135ba4d6278ab769d25365684c7d24365ad
Andre <20230331@ricaldone.edu.sv>
2025-05-19 19:06:37 -0600

    un gran commit

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,27 @@
 import cookieParser from "cookie-parser";
 import express from "express";
+import clienteRoutes from "./routes/clientes.js";
+import empleadoRoutes from "./routes/empleados.js";
+import loginRoutes from "./routes/login.js";
+import logoutRoutes from "./routes/logout.js";
+import peliculasRoutes from "./routes/peliculas.js";
+import recoveryPasswordRoutes from "./routes/recoveryPassword.js"; 
+import registerClienteRoutes from "./routes/registerCliente.js";
+import registerEmpleadoRoutes from "./routes/registerEmpleado.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use(cookieParser());
+
+app.use("/api/clientes", clienteRoutes);
+app.use("/api/empleados", empleadoRoutes);
+app.use("/api/login", loginRoutes);
+app.use("/api/logout", logoutRoutes);
+app.use("/api/peliculas", peliculasRoutes);
+app.use("/api/recoveryPassword", recoveryPasswordRoutes);
+app.use("/api/registerCliente", registerClienteRoutes);
+app.use("/api/registerEmpleado", registerEmpleadoRoutes);
+
+export default app;
